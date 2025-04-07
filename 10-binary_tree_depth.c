04c4c8485c9e6e3214820751456377da5f800350
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-07 14:35:40 +0000

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,13 +7,16 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth = binary_tree_depth(tree->left);
-        size_t right_depth = binary_tree_depth(tree->right);
+	size_t left_depth;
+	size_t right_depth;
 
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-		return (left_depth > right_depth ? left_depth : right_depth) +1;
-	}
+	left_depth = binary_tree_depth(tree->left);
+	right_depth = binary_tree_depth(tree->right);
+
+	return (left_depth > right_depth ? left_depth : right_depth) +1;
+}
