b98d23c2e84186f0a5f93e3ed43a955ad18ce595
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-07 14:16:09 +0000

    Height of the three

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_height - display the height of the tree
+ *
+ * Return: the left or the right height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return(0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (1 + left_height);
+	}
+	else
+	{
+		return (1 + right_height);
+	}
+}
