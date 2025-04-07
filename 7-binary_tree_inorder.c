5b6f16d9e2560bb92258166b1c3a76ec0016f17a
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-07 13:38:15 +0000

    In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_inorder - display the tree in order
+ *@tree: the three we want to display
+ *@func: a pointer to a function to call for each node
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
