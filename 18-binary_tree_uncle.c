cdb8db9a134314e0b3736be9555d1213780237b3
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-08 13:40:37 +0000

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_uncle - function that finds the uncle of a node
  *
- * Return: a pointer to the uncle node or null 
+ * Return: a pointer to the uncle node or null
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
@@ -12,19 +12,19 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node == NULL || node->parent == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	if (parent->parent == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	if (parent == parent->parent->left)
 	{
-		return parent->parent->right;
-	} 
+		return (parent->parent->right);
+	}
 	else
 	{
-		return parent->parent->left;
+		return (parent->parent->left);
 	}
 }
