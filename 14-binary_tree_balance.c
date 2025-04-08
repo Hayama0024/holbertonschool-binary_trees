95a2b88addc589f4b963f0fd1a31eca3f5946d57
Hayama0024 <hayamareizo11@gmail.com>
2025-04-08 12:26:10 +0000

    fix compile error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,12 +8,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int left, right;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
