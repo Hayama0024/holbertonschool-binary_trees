afcf3498c9b5b7a0594ac7018d84e47e8800d8ef
Hayama0024 <hayamareizo11@gmail.com>
2025-04-08 13:26:24 +0000

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,7 +27,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (1 + right_height);
 	}
 }
-#include "binary_trees.h"
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: The binary tree
